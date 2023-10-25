0a5c2a4f28e59e2e47bef3e0dab80863c6848bd4
ngolehuengan <ngolehuengan170203@gmail.com>
2023-10-25 13:08:45 +0700

    login form

--- GUI/Form1.cs
+++ GUI/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace GUI
 {
@@ -15,6 +16,36 @@ namespace GUI
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //thông báo lỗi chưa viết
+            MessageBox.Show("");
+        }
+
+        private void alphaBlendTextBox1_Leave(object sender, EventArgs e)
+        {
+            if (alphaBlendTextBox1.Text == "")
+                alphaBlendTextBox1.Text = "Tên đăng nhập";
+        }
+
+        private void alphaBlendTextBox1_Enter(object sender, EventArgs e)
+        {
+            if (alphaBlendTextBox1.Text == "Tên đăng nhập")
+                alphaBlendTextBox1.Text = "";
+        }
+
+        private void alphaBlendTextBox2_Leave(object sender, EventArgs e)
+        {
+            if (alphaBlendTextBox2.Text == "")
+                alphaBlendTextBox2.Text = "Mật khẩu";
+        }
+
+        private void alphaBlendTextBox2_Enter(object sender, EventArgs e)
+        {
+            if (alphaBlendTextBox2.Text == "Mật khẩu")
+                alphaBlendTextBox2.Text = "";
         }
     }
 }
