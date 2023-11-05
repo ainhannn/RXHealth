21bbdcbd44301b39c652028dd72cb8e7ddc2ec53
ngolehuengan <ngolehuengan170203@gmail.com>
2023-11-05 14:33:39 +0700

    chỉnh lại responsive tí

--- GUI/Application/Forms/Account/FormAccountStyle.cs
+++ GUI/Application/Forms/Account/FormAccountStyle.cs
@@ -6,7 +6,7 @@ namespace GUI
     {
         private void FormAccount_Resize(object sender, System.EventArgs e)
         {
-            form.Visible = Height > 900;
+            form.Visible = Height > 900 && Width > 1100;
             avtForm.Visible = Height > 700;
         }
     }
