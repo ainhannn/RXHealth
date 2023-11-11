2082e06c5818ec263f566f21470d56531460b86b
Ái Nhân <136802310+ainhannn@users.noreply.github.com>
2023-11-11 08:56:47 +0000

    Update insertdata.sql

--- database/insertdata.sql
+++ database/insertdata.sql
@@ -1,20 +1,28 @@
 -- Active: 1695203080273@@127.0.0.1@3306@pharmacy
 USE pharmacy;
 
-INSERT INTO account (username,password,role) VALUES
-    ('admin','123',1),
-    ('staff','123',2),
-    ('guest','123',3);
-INSERT INTO staff (nickname,citizen_id_number,fullname,birthday,qualification,contact_number,account_id) VALUES
-    ('Calcifer','003121410361','Trần Thụy Ái Nhân','2003-10-06','Đang học Đại học','0857288009',1),
-    ('Chanh','003121410292','Nguyễn Vủ Quốc Lâm','2003-10-08','Đang học Đại học','0941545482',2),
-    ('NLHN','003121410339','Ngô Lê Huệ Ngân','2003-02-17','Đang học Đại học','0869037600',3);
+INSERT INTO account (username,password,role) VALUE 
+	('admin','123',0),
+	('manager','123',1),
+	('stocker','123',2),
+	('seller','123',3);
+
+INSERT INTO staff (nickname,citizen_id_number,fullname,birthday,qualification,contact_number) VALUES
+    ('Calcifer','003121410361','Trần Thụy Ái Nhân','2003-10-06','Đang học Đại học','0857288009'),
+    ('Chanh','003121410292','Nguyễn Vủ Quốc Lâm','2003-10-08','Đang học Đại học','0941545482'),
+    ('NLHN','003121410339','Ngô Lê Huệ Ngân','2003-02-17','Đang học Đại học','0869037600');
+
+CALL insert_account(1,1);
+CALL insert_account(2,3);
+CALL insert_account(3,2);
+
 INSERT INTO customer (name,contact_number) VALUES
     ('Guest', ''),
     ('Duy Khang', '0358808913'),
     ('Kế Cường', '0336773026'),
     ('Lag', '0865674317'),
     ('Thư NT', '0368779041');
+
 INSERT INTO country(name) VALUES  
     ('Unknown'),
     ('Việt Nam'),
@@ -34,7 +42,8 @@ INSERT INTO country(name) VALUES
     ('Canada'),
     ('Hàn Quốc'),
     ('Hà Lan');
-INSERT INTO active_ingredient (name) VALUES
+
+INSERT INTO substance(name) VALUES
     ('Amlodipin'),
     ('Acid Salicylic'),
     ('Amoxicillin'),
@@ -54,6 +63,7 @@ INSERT INTO active_ingredient (name) VALUES
     ('Paracetamol'),
     ('Salbutamol'),
     ('Vitamin C');
+    
 INSERT INTO category (name) VALUES
     ('Thuốc kê đơn'),
     ('Thuốc không kê đơn'),
@@ -61,19 +71,13 @@ INSERT INTO category (name) VALUES
     ('Chăm sóc cá nhân'),
     ('Dụng cụ y tế'),
     ('Thiết bị y tế');
-INSERT INTO manufacturer (name) VALUES
-    ('Công ty TNHH Dược phẩm Hisamitsu Việt Nam');
-INSERT INTO unit (name) VALUES
-    ('viên 8mg'),
-    ('gói 3g'),
-    ('gói 10miếng'),
-    ('ống 5ml'),
-    ('ống 10ml'),
-    ('miếng'),
-    ('cái');
-INSERT INTO unit (name,number_each_unit,subunit_id) VALUES
-    ('hộp 30viên',30,1),
-    ('hộp 2gói',30,3);
+
+
+
+
+
+
+
 INSERT INTO product_info (code,name,category_id,manufacturer_id,expiry,unit_id,storage_condition,note) VALUES
     ('VD-22387-15','Salonpas',2,1,36,8,
     'Bảo quản nơi khô ráo, nhiệt độ 30 độ C, tránh ẩm ướt và ánh nắng trực tiếp. Đối với những miếng cao dán đã mở mà không dùng hết, nên bọc kín sản phẩm để tránh vi khuẩn xâm nhập. Không bảo quản thuốc trong phòng tắm hay ngăn đá để tránh ảnh hưởng đến chất lượng.',
