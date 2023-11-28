4efac0cab77f0f8504856c9324a78070885e5f50
Ái Nhân <136802310+ainhannn@users.noreply.github.com>
2023-11-28 15:42:42 +0700

    Update create.sql

--- database/create.sql
+++ database/create.sql
@@ -391,6 +391,8 @@ INNER JOIN sale_detail ON product.id=sale_detail.product_id
 GROUP BY product.barcode;
 
 -- -- -- -- -- VALUES -- -- -- -- --
+INSERT INTO setting_value VALUE (0.05, 0.15, 20000, 1);
+	
 INSERT INTO staff (nickname,citizen_id_number,fullname,birthday,qualification,contact_number) VALUES
     ('admin','000000000000','Admin','2023-11-20','Đang học Đại học','0123456789'),
     ('manager','000000000001','Manager','2023-11-20','Đang học Đại học','0123456789'),
